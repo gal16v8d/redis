1d790af18d790cc7c92d672d6988ad5449c71b45
Alex Galvis <alex.galvis.sistemas@gmail.com>
2021-12-14 11:42:31 -0500

    Update project structure

---
+++ src/main/java/com/gsdd/redis/dto/UserDTO.java
@@ -0,0 +1,16 @@
+package com.gsdd.redis.dto;
+
+import java.io.Serializable;
+import lombok.Data;
+import lombok.Generated;
+
+@Generated
+@Data
+public class UserDTO implements Serializable {
+
+  private static final long serialVersionUID = -552202647735887057L;
+  private String name;
+  private String lastName;
+  private LocationDTO location;
+
+}
